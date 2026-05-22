02af90f3eb2769667c2c44ad67350afec8958f8a
Dexter Montero <dexter.montero09@gmail.com>
2026-05-22 16:27:55 +0800

    feat: fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,21 @@
 import React from "react";
 import { ThemeProvider } from "next-themes";
 import { TooltipProvider } from "@/components/ui/tooltip";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Index from "@/pages/Index";
 import NotFound from "@/pages/NotFound";
 
-const queryClient = new QueryClient();
-
 const App: React.FC = () => (
     <ThemeProvider attribute="class" defaultTheme="light" enableSystem={false} disableTransitionOnChange={false}>
-        <QueryClientProvider client={ queryClient } >
-            <TooltipProvider>
-                <BrowserRouter>
-                    <Routes>
-                        <Route path="/" element={<Index />} />
-                        <Route path="*" element={<NotFound />} />
-                    </Routes>
-                </BrowserRouter>
-            </TooltipProvider>
-        </QueryClientProvider>
+        <TooltipProvider>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<Index />} />
+                    <Route path="*" element={<NotFound />} />
+                </Routes>
+            </BrowserRouter>
+        </TooltipProvider>
     </ThemeProvider>
 )
 
